370a312dbc8664b4710df3ba20d27511ecfcac20
Leo Sjöberg <leo@decahedron.io>
2018-04-27 16:56:32 +0100

    Release v1.4.0
    Squashed with the following commits:
    - Release v1.5.1: push dist folder
    - Release v1.5.2
    - Release v1.5.2: push dist folder
    - Release v1.5.3
    - build: removed dist from Git
    - build: added dist folder to gitignore

--- dist/src/Entity.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Entity {
-    private static jsonParse<T>(sourceObject, jsonObject);
-    fromJson(jsonData: any): any;
-    toJson(toSnake?: boolean, asString?: boolean): any;
-}
